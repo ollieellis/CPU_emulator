4ef189991507333cc8e79fcca52e9a1c701d6355
ES5017 <es5017@ic.ac.uk>
2018-10-28 23:52:20 +0000

    reading binary complete

--- src/helpers.cpp
+++ src/helpers.cpp
@@ -23,7 +23,7 @@ uint32_t *file_io::get_binary_file(string file_path)
     // while (size-- > 0)
     // {
     //     in >> current;
-    //     std::cout << std::hex << current;
+    //     std::cout << std::heto_swap << current;
     //     vec.push_back(current);
     // }
     //yusu
@@ -41,12 +41,18 @@ uint32_t *file_io::get_binary_file(string file_path)
         file.seekg(0, ios::beg);
         file.read(memblock, size);
         file.close();
+        int num = 14;
+        cout << "original: " << num << "  swapped: " << bitwise_helper::swap_bits(num, 2, 3) << endl;
 
         instructions = reinterpret_cast<uint32_t *>(memblock);
 
         for (size_t i = 0; i < size / sizeof(instructions[0]); i++)
         {
-            instructions[i] = file_io::rotate_left(instructions[i], 0);
+            cout << "before: " << hex << uppercase << instructions[i] << endl;
+
+            instructions[i] = bitwise_helper::swap_bytes(instructions[i], 0, 3);
+            instructions[i] = bitwise_helper::swap_bytes(instructions[i], 1, 2);
+
             cout << "instruction: ";
             cout << hex << uppercase << instructions[i] << std::endl;
         }
@@ -62,7 +68,7 @@ uint32_t *file_io::get_binary_file(string file_path)
 }
 
 template <class T>
-inline T file_io::rotate_left(T v, std::int32_t shift)
+inline T bitwise_helper::rotate_left(T v, std::int32_t shift)
 {
     std::size_t m = sizeof(v) * std::numeric_limits<T>::digits;
     T s = shift >= 0 ? shift % m : -((-shift) % m);
@@ -70,9 +76,40 @@ inline T file_io::rotate_left(T v, std::int32_t shift)
 }
 
 template <class T>
-inline T file_io::rotate_right(T v, std::int32_t shift)
+inline T bitwise_helper::rotate_right(T v, std::int32_t shift)
 {
     std::size_t m = sizeof(v) * std::numeric_limits<T>::digits;
     T s = shift >= 0 ? shift % m : -((-shift) % m);
     return (v >> s) | (v << (m - s));
+}
+
+// This function swaps bit at positions p1 and p2 in an integer n
+unsigned int bitwise_helper::swap_bits(unsigned int n, unsigned int p1, unsigned int p2)
+{
+    /* Move p1'th to rightmost side */
+    unsigned int bit1 = (n >> p1) & 1;
+
+    /* Move p2'th to rightmost side */
+    unsigned int bit2 = (n >> p2) & 1;
+
+    /* XOR the two bits */
+    unsigned int x = (bit1 ^ bit2);
+
+    /* Put the xor bit back to their original positions */
+    x = (x << p1) | (x << p2);
+
+    /* XOR 'x' with the original number so that the 
+       two sets are swapped */
+    unsigned int result = n ^ x;
+    return result;
+}
+
+unsigned int bitwise_helper::swap_bytes(unsigned int n, unsigned int p1, unsigned int p2)
+{
+    unsigned int result = n;
+    for (size_t i = 0; i < 8; i++)
+    {
+        result = bitwise_helper::swap_bits(result, p1 * 8 + i, p2 * 8 + i);
+    }
+    return result;
 }
--- src/helpers.hpp
+++ src/helpers.hpp
@@ -6,8 +6,15 @@ class file_io
 {
   public:
     static uint32_t* get_binary_file(string file_path);
+}; 
+
+class bitwise_helper
+{
+  public:
     template< class T>
     static inline T rotate_left( T v, std::int32_t shift);
     template< class T>
     static inline T rotate_right( T v, std::int32_t shift);
-}; 
+    static unsigned int swap_bits(unsigned int n, unsigned int p1, unsigned int p2) ; 
+    static unsigned int swap_bytes(unsigned int n, unsigned int p1, unsigned int p2);
+};
