6c3c87ac8c069cd18e6bdc55a129f59e533ff908
ES5017 <es5017@ic.ac.uk>
2018-11-19 16:53:41 +0000

    fixed weird ascii thing

--- test/mips_assembly/lwl2.mips.s
+++ test/mips_assembly/lwl2.mips.s
@@ -1,18 +1,18 @@
 #author: ES5017
-#expected_output: 205 103
+#expected_output: 170 103
 #extra_info: tests lwl aligned
 
 lui $1, 0x2000
 
 lui $3, 0x6789
-ori $3, $3, 0xABCD
-#we have in $3 0x6789ABCD
+ori $3, $3, 0xABAA
+#we have in $3 0x6789ABAA
 
 sw $3, 0($1)
 
 addi $2, $0, 0x1234
 lwl $2, 0($1)
-#in $2 we have 0x6789ABCD
+#in $2 we have 0x6789ABAA
 
 
 lui $9, 0x3000
--- test/mips_assembly/lwr1.mips.s
+++ test/mips_assembly/lwr1.mips.s
@@ -1,12 +1,12 @@
 #author: ES5017
-#expected_output: 137 103 205 171
+#expected_output: 137 103 170 171
 #extra_info: tests lwl with lwr and register merge is correct
 
 lui $1, 0x2000
 
 lui $3, 0x6789
-ori $3, $3, 0xABCD
-#we have in $3 0x6789ABCD
+ori $3, $3, 0xABAA
+#we have in $3 0x6789ABAA
 
 sw $3, 0($1)
 sw $3, 4($1)
@@ -18,7 +18,7 @@ lwl $9, 2($1)
 nop
 lwr $9, 5($1) 
 
-#hte value in $9 should be 0xABCD6789
+#hte value in $9 should be 0xABAA6789
 
 lui $10, 0x3000
 
