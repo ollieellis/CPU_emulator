b28c244b0383e794892b2348f02c72b179301651
ES5017 <es5017@ic.ac.uk>
2018-11-20 01:24:05 +0000

    cleaned up final

--- make_parser.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-cd test/mips-parser
-make parser
--- run.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-echo "run.sh started \n" #comment this out later
-make simulator
-#sh test/convert_mips.sh #done in testbench
-make testbench
-bin/mips_testbench bin/mips_simulator
